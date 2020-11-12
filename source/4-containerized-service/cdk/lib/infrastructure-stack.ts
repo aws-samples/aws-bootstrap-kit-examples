34788263aef96584e5d4c2bdccd9f411830174d6
Ahmed Elsenousi <aelsenou@amazon.com>
2020-11-12 14:09:55 +0000

    tagging

--- source/4-containerized-service/cdk/lib/infrastructure-stack.ts
+++ source/4-containerized-service/cdk/lib/infrastructure-stack.ts
@@ -1,28 +1,28 @@
-import { Stack, StackProps, Construct } from "@aws-cdk/core";
-import { Vpc } from "@aws-cdk/aws-ec2";
+import { Stack, StackProps, Construct, Tags } from '@aws-cdk/core';
+import { Vpc } from '@aws-cdk/aws-ec2';
 import {
   Cluster,
   ContainerImage,
   AwsLogDriver,
   FargateTaskDefinition,
-  Secret,
-} from "@aws-cdk/aws-ecs";
-import { ApplicationLoadBalancedFargateService } from "@aws-cdk/aws-ecs-patterns";
+  Secret
+} from '@aws-cdk/aws-ecs';
+import { ApplicationLoadBalancedFargateService } from '@aws-cdk/aws-ecs-patterns';
 import {
   Dashboard,
   GraphWidget,
   TextWidget,
   LogQueryVisualizationType,
-  LogQueryWidget,
-} from "@aws-cdk/aws-cloudwatch";
-import { DatabaseClusterEngine, ServerlessCluster } from "@aws-cdk/aws-rds";
-import { CloudFrontToS3 } from "@aws-solutions-constructs/aws-cloudfront-s3";
+  LogQueryWidget
+} from '@aws-cdk/aws-cloudwatch';
+import { DatabaseClusterEngine, ServerlessCluster } from '@aws-cdk/aws-rds';
+import { CloudFrontToS3 } from '@aws-solutions-constructs/aws-cloudfront-s3';
 import {
   AwsCustomResource,
   AwsCustomResourcePolicy,
-  PhysicalResourceId,
-} from "@aws-cdk/custom-resources";
-import { BucketDeployment, Source } from "@aws-cdk/aws-s3-deployment";
+  PhysicalResourceId
+} from '@aws-cdk/custom-resources';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
 
 export class InfrastructureStack extends Stack {
   public readonly loadBalancer: any;
@@ -30,65 +30,67 @@ export class InfrastructureStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const vpc = new Vpc(this, "MyVpc", { maxAzs: 2 });
+    Tags.of(this).add('ServiceName', this.node.tryGetContext('service_name'));
+
+    const vpc = new Vpc(this, 'MyVpc', { maxAzs: 2 });
 
     ////////// CLOUDFRONT //////////////
 
-    const cloudFront = new CloudFrontToS3(this, "my-cloudfront-s3", {});
+    const cloudFront = new CloudFrontToS3(this, 'my-cloudfront-s3', {});
 
     // prepopulate bucket with a few images
-    new BucketDeployment(this, "DeployS3Images", {
-      sources: [Source.asset("./static")],
+    new BucketDeployment(this, 'DeployS3Images', {
+      sources: [Source.asset('./static')],
       destinationBucket: cloudFront.s3Bucket!,
-      destinationKeyPrefix: "static",
+      destinationKeyPrefix: 'static'
     });
     const staticDomain =
-      cloudFront.cloudFrontWebDistribution.distributionDomainName + "/static";
+      cloudFront.cloudFrontWebDistribution.distributionDomainName + '/static';
 
     ////////// Database ////////////
 
-    const db = new ServerlessCluster(this, "MyDatabase", {
+    const db = new ServerlessCluster(this, 'MyDatabase', {
       engine: DatabaseClusterEngine.AURORA_MYSQL,
-      defaultDatabaseName: "ecommerce",
+      defaultDatabaseName: 'ecommerce',
       enableHttpEndpoint: true,
-      vpc,
+      vpc
     });
 
     // prepopulate the Database with a few products
-    const createTable = new AwsCustomResource(this, "CreateTable", {
+    const createTable = new AwsCustomResource(this, 'CreateTable', {
       onCreate: {
-        service: "RDSDataService",
-        action: "executeStatement",
+        service: 'RDSDataService',
+        action: 'executeStatement',
         parameters: {
           resourceArn: db.clusterArn,
           secretArn: db.secret?.secretArn,
-          database: "ecommerce",
+          database: 'ecommerce',
           sql:
-            "CREATE TABLE products ( productId int, name varchar(255), image varchar(255), price decimal(5, 2) );",
+            'CREATE TABLE products ( productId int, name varchar(255), image varchar(255), price decimal(5, 2) );'
         },
-        physicalResourceId: PhysicalResourceId.of(Date.now().toString()),
+        physicalResourceId: PhysicalResourceId.of(Date.now().toString())
       },
       policy: AwsCustomResourcePolicy.fromSdkCalls({
-        resources: AwsCustomResourcePolicy.ANY_RESOURCE,
-      }),
+        resources: AwsCustomResourcePolicy.ANY_RESOURCE
+      })
     });
     db.secret?.grantRead(createTable);
 
-    const insertTable = new AwsCustomResource(this, "InsertTable", {
+    const insertTable = new AwsCustomResource(this, 'InsertTable', {
       onCreate: {
-        service: "RDSDataService",
-        action: "executeStatement",
+        service: 'RDSDataService',
+        action: 'executeStatement',
         parameters: {
           resourceArn: db.clusterArn,
           secretArn: db.secret?.secretArn,
-          database: "ecommerce",
-          sql: `INSERT INTO products VALUES ( 1, 'hat', 'https://${staticDomain}/hat.jpeg', 12.55), ( 2, 'shoe', 'https://${staticDomain}/shoe.jpg', 19.85);`,
+          database: 'ecommerce',
+          sql: `INSERT INTO products VALUES ( 1, 'hat', 'https://${staticDomain}/hat.jpeg', 12.55), ( 2, 'shoe', 'https://${staticDomain}/shoe.jpg', 19.85);`
         },
-        physicalResourceId: PhysicalResourceId.of(Date.now().toString()),
+        physicalResourceId: PhysicalResourceId.of(Date.now().toString())
       },
       policy: AwsCustomResourcePolicy.fromSdkCalls({
-        resources: AwsCustomResourcePolicy.ANY_RESOURCE,
-      }),
+        resources: AwsCustomResourcePolicy.ANY_RESOURCE
+      })
     });
     db.secret?.grantRead(insertTable);
 
@@ -96,40 +98,40 @@ export class InfrastructureStack extends Stack {
 
     ////////// ECS ////////////
 
-    const cluster = new Cluster(this, "MyCluster", { vpc });
+    const cluster = new Cluster(this, 'MyCluster', { vpc });
 
-    const NGINXLogDriver = new AwsLogDriver({ streamPrefix: "myNGINX" });
-    const PHPLogDriver = new AwsLogDriver({ streamPrefix: "myPHP" });
+    const NGINXLogDriver = new AwsLogDriver({ streamPrefix: 'myNGINX' });
+    const PHPLogDriver = new AwsLogDriver({ streamPrefix: 'myPHP' });
 
-    const taskDefinition = new FargateTaskDefinition(this, "TaskDef");
+    const taskDefinition = new FargateTaskDefinition(this, 'TaskDef');
 
-    const nginxContainer = taskDefinition.addContainer("ab-nginx", {
-      image: ContainerImage.fromAsset(__dirname + "/../../nginx"),
-      logging: NGINXLogDriver,
+    const nginxContainer = taskDefinition.addContainer('ab-nginx', {
+      image: ContainerImage.fromAsset(__dirname + '/../../nginx'),
+      logging: NGINXLogDriver
     });
     nginxContainer.addPortMappings({ containerPort: 80 });
 
     const fargateService = new ApplicationLoadBalancedFargateService(
       this,
-      "MyFargateService",
+      'MyFargateService',
       {
         cluster: cluster,
         desiredCount: 2,
         taskDefinition,
-        publicLoadBalancer: true,
+        publicLoadBalancer: true
       }
     );
     this.loadBalancer = fargateService.loadBalancer;
 
-    const phpContainer = taskDefinition.addContainer("ab-php", {
-      image: ContainerImage.fromAsset(__dirname + "/../../php-fpm"),
+    const phpContainer = taskDefinition.addContainer('ab-php', {
+      image: ContainerImage.fromAsset(__dirname + '/../../php-fpm'),
       environment: {
-        DOMAIN: "http://" + fargateService.loadBalancer.loadBalancerDnsName,
+        DOMAIN: 'http://' + fargateService.loadBalancer.loadBalancerDnsName
       },
       secrets: {
-        SECRETS: Secret.fromSecretsManager(db.secret!),
+        SECRETS: Secret.fromSecretsManager(db.secret!)
       },
-      logging: PHPLogDriver,
+      logging: PHPLogDriver
     });
     phpContainer.addPortMappings({ containerPort: 9000 });
 
@@ -137,41 +139,41 @@ export class InfrastructureStack extends Stack {
 
     /////////////////// CloudWatch DashBoard /////////////////////
 
-    const dashboard = new Dashboard(this, "MyDashboard");
+    const dashboard = new Dashboard(this, 'MyDashboard');
     dashboard.addWidgets(
       new TextWidget({
         markdown:
-          "# Load Balancer\nmetrics to monitor load balancer metrics:\n* Amount of incoming requests\n* Latency with an alarm if max accepted latency exceeded.",
+          '# Load Balancer\nmetrics to monitor load balancer metrics:\n* Amount of incoming requests\n* Latency with an alarm if max accepted latency exceeded.',
         width: 6,
-        height: 6,
+        height: 6
       }),
       new GraphWidget({
-        title: "Requests",
+        title: 'Requests',
         width: 9,
-        left: [fargateService.loadBalancer.metricRequestCount()],
+        left: [fargateService.loadBalancer.metricRequestCount()]
       }),
       new GraphWidget({
-        title: "Latency",
+        title: 'Latency',
         width: 9,
-        left: [fargateService.loadBalancer.metricTargetResponseTime()],
+        left: [fargateService.loadBalancer.metricTargetResponseTime()]
       })
     );
     dashboard.addWidgets(
       new LogQueryWidget({
-        title: "NGINX Logs",
+        title: 'NGINX Logs',
         width: 24,
         logGroupNames: [NGINXLogDriver.logGroup?.logGroupName!],
         view: LogQueryVisualizationType.TABLE,
-        queryLines: ["fields @timestamp, @message"],
+        queryLines: ['fields @timestamp, @message']
       })
     );
     dashboard.addWidgets(
       new LogQueryWidget({
-        title: "PHP Logs",
+        title: 'PHP Logs',
         width: 24,
         logGroupNames: [PHPLogDriver.logGroup?.logGroupName!],
         view: LogQueryVisualizationType.TABLE,
-        queryLines: ["fields @timestamp, @message"],
+        queryLines: ['fields @timestamp, @message']
       })
     );
   }
--- source/4-containerized-service/cdk/test/infrastructure.test.ts
+++ source/4-containerized-service/cdk/test/infrastructure.test.ts
@@ -1,13 +1,32 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as Infrastructure from '../lib/infrastructure-stack';
+import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import { App } from '@aws-cdk/core';
+import { InfrastructureStack } from '../lib/infrastructure-stack';
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new Infrastructure.InfrastructureStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test('Stack is Tagged', () => {
+  const app = new App({
+    context: {
+      service_name: 'Ecommerce-App'
+    }
+  });
+  const stack = new InfrastructureStack(app, 'MyTestStack');
+  expectCDK(stack).to(
+    haveResource('AWS::S3::Bucket', {
+      Tags: [
+        {
+          Key: 'ServiceName',
+          Value: 'Ecommerce-App'
+        }
+      ]
+    })
+  );
+  expectCDK(stack).to(
+    haveResource('AWS::RDS::DBCluster', {
+      Tags: [
+        {
+          Key: 'ServiceName',
+          Value: 'Ecommerce-App'
+        }
+      ]
+    })
+  );
 });
