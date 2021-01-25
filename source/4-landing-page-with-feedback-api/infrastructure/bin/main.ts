9a41804907842c206eb11ab3a08d47878da656fd
Florian CHAZAL <chazalf@amazon.com>
2021-01-25 15:09:08 +0100

    add proper permission boundary for cicd pipeline

--- source/4-landing-page-with-feedback-api/infrastructure/bin/main.ts
+++ source/4-landing-page-with-feedback-api/infrastructure/bin/main.ts
@@ -4,9 +4,14 @@ import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { InfrastructureStack } from "../lib/infrastructure-stack";
 // import { LandingPagePipelineStack } from "../lib/cicd-pipeline-stack";
+// import { AddPermissionsBoundaryToRoles } from "../lib/permission-boundary";
 
 const app = new cdk.App();
 
 new InfrastructureStack(app, 'LandingPageStack-dev', {});
 
-// new LandingPagePipelineStack(app, 'LandingPagePipelineStack');
+// const pipelineStack = new LandingPagePipelineStack(app, 'LandingPagePipelineStack');
+
+// // Respect cdk bootstrap policy insuring pipelines construct can't create more than what it needs for CI/CD pipeline creation
+// const permissionBoundaryArn = cdk.Fn.importValue('CICDPipelinePermissionsBoundaryArn');
+// cdk.Aspects.of(pipelineStack).add(new AddPermissionsBoundaryToRoles(permissionBoundaryArn));
