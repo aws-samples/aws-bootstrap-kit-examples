3f60f552252b5e51806211e06b30be4cffa57c7a
Dylan O'Reagan <dooreaga@amazon.com>
2021-02-01 15:38:48 +0000

    Add github url for node version 15.6 error to provide users with context

--- source/utils/versions.ts
+++ source/utils/versions.ts
@@ -1,3 +1,5 @@
+// Issues with asset upload on version 15.6 of node mean we restrict the node version to a specific range.
+// See https://github.com/aws/aws-cdk/issues/12536 for more context
 export function ensure_correct_node_version() {
     const version_arr = process.versions.node.split('.').map(Number)
     if ((version_arr[0] >= 15 && version_arr[1] >= 6) || version_arr[0] < 12) {
