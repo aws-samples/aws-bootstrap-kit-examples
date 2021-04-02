de6914fee50ca1d0176f8f7e43d59ba9b380856b
cfd-administrator-aws <aws-organization-admin@confledis.com>
2021-04-02 12:05:12 +0100

    deleting the 3 and 4 folders under source

--- source/2-landing-page/cdk/bin/landing-page.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { LandingPageStack } from '../lib/landing-page-stack';
-
-const app = new cdk.App();
-
-new LandingPageStack(app, 'LandingPageStack');
--- source/4-landing-page-with-feedback-api/infrastructure/test/backend.test.ts
+++
@@ -1,9 +0,0 @@
-test('Empty Stack', () => {
-    // const app = new cdk.App();
-    // // WHEN
-    // const stack = new Backend.BackendStack(app, 'MyTestStack', );
-    // // THEN
-    // expectCDK(stack).to(matchTemplate({
-    //   "Resources": {}
-    // }, MatchStyle.EXACT))
-});
