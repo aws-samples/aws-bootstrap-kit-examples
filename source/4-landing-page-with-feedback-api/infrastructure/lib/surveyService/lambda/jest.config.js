67ef1e4734fae3c42a403dcaeed1e76ebbe3f482
Florian Chazal <florianchazal@gmail.com>
2021-07-19 10:28:56 +0200

    cdk 1.114.0 / bootstrap v8 (#80)
    * bump bk-kit to 0.3.13 and cdk 1.114.0
    
    * revert change on used profile for admin
    
    * update lock files
    
    Co-authored-by: Florian CHAZAL <chazalf@amazon.com>

--- source/4-landing-page-with-feedback-api/infrastructure/lib/surveyService/lambda/jest.config.js
+++ source/4-landing-page-with-feedback-api/infrastructure/lib/surveyService/lambda/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ['<rootDir>/test', '<rootDir>/lib/surveyService/lambda/'],
+  roots: ['<rootDir>/test'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
