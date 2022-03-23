e1ec9d7f770e071e4fa6719a892592c849da4d8c
Dmitry Balabanov <balabdmi@amazon.de>
2022-03-23 19:31:34 +0100

    Fix incorrect module reference (#158)

--- source/5-unicornPics/loadtesting/test/loadtesting.test.ts
+++ source/5-unicornPics/loadtesting/test/loadtesting.test.ts
@@ -1,6 +1,6 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as Loadtesting from '../lib/loadtesting-stack';
+import * as Loadtesting from '../lib/loadtesting';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
